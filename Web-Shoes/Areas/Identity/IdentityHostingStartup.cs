ecbe407864f0f0816ec1a8e23afb62bf3d9a4b19
Bao <Bao@DESKTOP-7B3G7RJ>
2021-08-09 19:10:39 +0700

    Add project files.

---
+++ Web-Shoes/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Web_Shoes.Data;
+
+[assembly: HostingStartup(typeof(Web_Shoes.Areas.Identity.IdentityHostingStartup))]
+namespace Web_Shoes.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
