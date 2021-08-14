b0bc73a948f8eaccee216bd244c2707e58931ddd
thaibao0225 <thaibao0225@gmail.com>
2021-08-15 04:44:07 +0700

    Done Front end

--- Web-Shoes/Entity/Categories.cs
+++ Web-Shoes/Entity/Categories.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Web_Shoes.Models
+namespace Web_Shoes.Entity
 {
-    public class ProductsModel
+    public class Categories
     {
     }
 }
