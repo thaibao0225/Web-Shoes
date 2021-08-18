b39530f51a2efc764892dded78dfca932c72fac4
thaibao0225 <thaibao0225@gmail.com>
2021-08-18 17:57:55 +0700

    Done index role management

--- Web-Shoes/Controllers/RoleManagementController.cs
+++ Web-Shoes/Controllers/RoleManagementController.cs
@@ -4,15 +4,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Shoes.Data;
 
 namespace Web_Shoes.Controllers
 {
     public class RoleManagementController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+
+        public RoleManagementController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: RoleManagementController
+        [Route("/rolemanagement")]
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+
+            var roleQuery = from a in _context.AppRole select a;
+            return View(roleQuery);
         }
 
         // GET: RoleManagementController/Details/5
