d6bc5203075d0a8e74bbd0cd253d22fd7384950d
thaibao0225 <thaibao0225@gmail.com>
2021-08-20 16:22:19 +0700

    Done front end home, men, wonmen and all product

--- Web-Shoes/Controllers/MenController.cs
+++ Web-Shoes/Controllers/MenController.cs
@@ -3,14 +3,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Shoes.Data;
 
 namespace Web_Shoes.Controllers
 {
     public class MenController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public MenController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var MenQuery = from a in _context.Products select a;
+
+            return View(MenQuery);
         }
+
+
     }
 }
--- Web-Shoes/Controllers/WomenController.cs
+++ Web-Shoes/Controllers/WomenController.cs
@@ -3,14 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Shoes.Data;
 
 namespace Web_Shoes.Controllers
 {
     public class WomenController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        public WomenController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            var WonmenQuery = from a in _context.Products select a;
+
+            return View(WonmenQuery);
         }
     }
 }
